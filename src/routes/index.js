949735b35f0fd20fb1cbf241fd06a6ab70018a02
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-11-24 13:28:34 -0300

    add inventory route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
+import { listProducts, addProduct } from './controllers/inventory';
 import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
@@ -7,5 +8,8 @@ const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
 indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
+
+indexRouter.get('/inventory', listProducts);
+indexRouter.post('/inventory', addProduct);
 
 export default indexRouter;
