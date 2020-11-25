f8b316a6526b5258f124161b32ee45fbdd1a9032
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-11-25 15:47:08 -0300

    add put and delete methods in routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
-import { listProducts, addProduct } from '../controllers';
+import { listProducts, addProduct, updateProduct, deleteProduct } from '../controllers';
 import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
@@ -11,5 +11,7 @@ indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 indexRouter.get('/inventory', listProducts);
 indexRouter.post('/inventory', addProduct);
+indexRouter.put('/inventory', updateProduct);
+indexRouter.delete('/inventory', deleteProduct);
 
 export default indexRouter;
