ac3195796dde86c8f3b9cce6ce50ea6fd9ce869d
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-11-25 14:52:20 -0300

    add methods for updating and deleting to model

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -13,4 +13,31 @@ export class InventoryModel extends BaseModel {
     `;
     return this.pool.query(query, values);
   }
+
+  async updateProductInfo(id, data) {
+    const columns = [];
+    const values = [id];
+
+    Object.keys(data).forEach((column, i) => {
+      if (data[column] !== undefined) {
+        columns.push(`${column} = $${i + 2}`);
+        values.push(data[column]);
+      }
+    });
+
+    const query = `
+      update ${this.table}
+      set ${columns.join(', ')}
+      where id = $1
+    `;
+
+    return this.pool.query(query, values);
+  }
+
+  async deleteProduct(productId) {
+    const query = `
+      DELETE FROM ${this.table} WHERE id = $1
+    `;
+    return this.pool.query(query, productId);
+  }
 }
