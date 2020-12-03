ccab0e6689f05690c157604301ab80b4dadfd9b3
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-03 10:29:47 -0300

    remove messages endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,17 @@ import messagesRouter from './messages';
 import purchaseRouter from './purchase';
 import searchProductRouter from './searchProduct';
 import shoppingCartRouter from './shoppingCart';
+import signUpRouter from './signUp';
+import loginRouter from './login';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.use('/inventory', inventoryRouter);
-indexRouter.use('/messages', messagesRouter);
 indexRouter.use('/shoppingCart', shoppingCartRouter);
 indexRouter.use('/purchase', purchaseRouter);
 indexRouter.use('/searchProduct', searchProductRouter);
+indexRouter.use('/signUp', signUpRouter);
+indexRouter.use('/login', loginRouter);
 
 export default indexRouter;
