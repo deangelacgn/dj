c50ac3e6ee272ece7c234f0ebb771d461435b152
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-07 13:19:09 -0300

    replace hooks

---
+++ test/utils.js
@@ -0,0 +1,33 @@
+import { createTables, dropTables } from "../src/migrations/queryFunctions";
+import { agent, BASE_URL } from './setup';
+
+export const clearDatabase = () => {
+  before(async function () {
+    this.timeout(10000);
+    await createTables();
+  });
+
+  after(async function() {
+    this.timeout(10000);
+    await dropTables();
+  });
+};
+
+export const createUser = (setAuthToken) => {
+  before(async function() {
+    const registerData = {
+      username: 'janedoe',
+      email: 'janedoe@somemail.com',
+      password: '12345',
+    };
+
+    await agent.post(`${BASE_URL}/user`).expect(200).send(registerData);
+
+    const loginData = {
+      user_login: 'janedoe',
+      password: '12345',
+    };
+    const authResponse = await agent.post(`${BASE_URL}/login`).expect(200).send(loginData);
+    setAuthToken(authResponse.body.token);
+  });
+};
