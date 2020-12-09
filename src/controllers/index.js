b0d423e4bc2544f9fefeb51315f7f3b3981e9ba5
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-09 14:14:30 -0300

    fix shopping cart tests

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,5 +2,4 @@ export * from './home';
 export * from './products';
 export * from './shoppingCart';
 export * from './purchase';
-export * from './searchProduct';
 export * from './users';
--- src/migrations/shoppingCart.js
+++ src/migrations/shoppingCart.js
@@ -1,5 +1,4 @@
 export const createShoppingCartTable = `
-  DROP TABLE IF EXISTS shopping_cart;
   CREATE TABLE IF NOT EXISTS shopping_cart (
     product_id INTEGER UNIQUE NOT NULL REFERENCES products (id),
     quantity INTEGER NOT NULL
