7459ec96a9876ac15a7f0a90a285c3232fda9931
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-09 16:29:20 -0300

    create aux function for validating password

--- src/validation/user.js
+++ src/validation/user.js
@@ -1,11 +1,18 @@
 import Joi from 'joi';
 
+const validatePassword = () => {
+  return Joi
+    .string()
+    .pattern(new RegExp('^[a-zA-Z0-9!"#$%&\'*\\(\\)=~+><?\\{\\}]{3,30}$'))
+    .required();
+};
+
 export const userRegistrationSchema = Joi.object({
   username: Joi.string().alphanum().min(2).max(30).required(),
   email: Joi.string().email({
     minDomainSegments: 2,
     tlds: { allow: ['com', 'br', 'net'] } }).required(),
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  password: validatePassword(),
 });
 
 export const loginSchema = Joi.object({
@@ -15,14 +22,14 @@ export const loginSchema = Joi.object({
       minDomainSegments: 2,
       tlds: { allow: ['com', 'br', 'net'] } }).required(),
   ],
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  password: validatePassword(),
 });
 
 export const changePasswordSchema = Joi.object({
   id: Joi.number(),
-  current_password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  new_password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-  repeat_password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+  current_password: validatePassword(),
+  new_password: validatePassword(),
+  repeat_password: validatePassword(),
 });
 
 export const deleteUserSchema = Joi.object({
