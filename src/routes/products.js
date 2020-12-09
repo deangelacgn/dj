894ba158ca7c5137a462457f4ecae35185570199
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-09 14:12:51 -0300

    update search product route

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,16 @@
 import express from 'express';
-import { listProducts, addProduct, updateProduct, deleteProduct } from '../controllers';
+import { 
+  listProducts,
+  addProduct, 
+  updateProduct, 
+  deleteProduct, 
+  searchProduct } from '../controllers';
+
 
 const productsRouter = express.Router();
 
 productsRouter.get('/',listProducts);
+productsRouter.get('/search', searchProduct);
 productsRouter.post('/', addProduct);
 productsRouter.patch('/', updateProduct);
 productsRouter.delete('/', deleteProduct);
