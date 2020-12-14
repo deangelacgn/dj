27d81a78b33f0d9e1deef2fa82cf4250f87a2422
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-14 15:03:25 -0300

    add validation to new routes

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { loginUser, sendEmailForgotPassword, resetPassword } from '../controllers';
+import { validateRequest } from '../middleware';
+import { sendEmailSecretCodeSchema, validateSecretCodeSchema } from '../validation';
 
 const loginRouter = express.Router();
 
 loginRouter.post('/', loginUser);
-loginRouter.post('/forgotPassword', sendEmailForgotPassword);
-loginRouter.post('/resetPassword', resetPassword);
+loginRouter.post('/forgotPassword',
+  validateRequest(sendEmailSecretCodeSchema, 'body'),
+  sendEmailForgotPassword);
+loginRouter.post('/resetPassword',
+  validateRequest(validateSecretCodeSchema, 'body'),
+  resetPassword);
 
 export default loginRouter;
--- src/validation/index.js
+++ src/validation/index.js
@@ -2,3 +2,4 @@ export * from './products';
 export * from './purchase';
 export * from './shoppingCart';
 export * from './user';
+export * from './secretCodes';
