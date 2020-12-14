e8f02cf9342732101ed3514072d26f4582a19709
deangelacgn@gmail.com <deangelacgn@gmail.com>
2020-12-14 14:57:21 -0300

    add forgot password to login routes

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { loginUser } from '../controllers';
+import { loginUser, sendEmailForgotPassword, resetPassword } from '../controllers';
 
 const loginRouter = express.Router();
 
 loginRouter.post('/', loginUser);
+loginRouter.post('/forgotPassword', sendEmailForgotPassword);
+loginRouter.post('/resetPassword', resetPassword);
 
 export default loginRouter;
